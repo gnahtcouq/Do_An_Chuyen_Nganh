dda81a55614a4a2f7de954573bb0253989dc6b44
Quoc Thang <comehere.thang@gmail.com>
2023-11-06 14:44:54 +0700

    Tạo Tất Cả Models, Migrations và Seeders

---
+++ migrations/migration-create-groomer-clinic-specialty.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.createTable('groomer_clinic_specialty', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            groomerId: {
+                type: Sequelize.INTEGER
+            },
+            clinicId: {
+                type: Sequelize.INTEGER
+            },
+            specialtyId: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async(queryInterface, Sequelize) => {
+        await queryInterface.dropTable('groomer_clinic_specialty');
+    }
+};
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -3,9 +3,14 @@
 module.exports = {
     up: async(queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Users', [{
+            email: 'comehere.thang@gmail.com',
+            password: '123@1234',
             firstName: 'Thang',
             lastName: 'Tran',
-            email: 'comehere.thang@gmail.com',
+            address: '99A Tam Danh, P4, Q8, HCM',
+            gender: 1,
+            typeRole: 'ROLE',
+            keyRole: 'R1',
             createdAt: new Date(),
             updatedAt: new Date()
         }]);
