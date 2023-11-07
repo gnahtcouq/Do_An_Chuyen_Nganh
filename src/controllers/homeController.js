514db1c99a068e3c89a0249d88d4a12fb4b1ca8b
Quoc Thang <comehere.thang@gmail.com>
2023-11-07 13:31:29 +0700

    CRUD với Node.js và Sequelize - Sử Dụng ORM Để Sửa Dữ liệu (Edit)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -36,10 +36,32 @@ let displayGetCRUD = async(req, res) => {
     })
 }
 
+let getEditCRUD = async(req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        // check userData not found
+
+        // let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else
+        return res.send("User not found!");
+}
+
+let putCRUD = async(req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.redirect("/get-crud");
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
