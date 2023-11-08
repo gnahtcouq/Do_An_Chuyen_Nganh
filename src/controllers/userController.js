dfb3db90a6d2b55bde789609708ab4dacfc780ee
Quoc Thang <comehere.thang@gmail.com>
2023-11-09 01:04:01 +0700

    Hoàn Thiện Chức Năng Đăng Nhập Kết Hợp React.Js Và Node.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,6 @@ import userService from '../services/userService'
 
 let handleLogin = async (req, res) => {
   let email = req.body.email
-  console.log('Your email ' + email)
   let password = req.body.password
 
   if (!email || !password) {
