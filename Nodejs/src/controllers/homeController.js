2bdd28cd06d2711a232d9b33609e67f70f54ef1d
Quoc Thang <comehere.thang@gmail.com>
2023-11-28 11:01:05 +0700

    Viết API Thêm, Sửa, Xóa Người Dùng
    Viết API Thêm, Sửa, Xóa Người Dùng

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -1,74 +1,74 @@
-import db from '../models/index';
-import CRUDService from '../services/CRUDService';
+import db from '../models/index'
+import CRUDService from '../services/CRUDService'
 
-let getHomePage = async(req, res) => {
-    try {
-        let data = await db.User.findAll();
-        return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
-        });
-    } catch (e) {
-        console.log(e);
-    }
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll()
+    return res.render('homepage.ejs', {
+      data: JSON.stringify(data)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
+  return res.render('test/about.ejs')
 }
 
 let getCRUD = (req, res) => {
-    return res.render('crud.ejs');
+  return res.render('crud.ejs')
 }
 
-let postCRUD = async(req, res) => {
-    let message = await CRUDService.createNewUser(req.body);
-    console.log(message);
-    return res.send("Post crud from server");
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body)
+  console.log(message)
+  return res.send('Post crud from server')
 }
 
-let displayGetCRUD = async(req, res) => {
-    let data = await CRUDService.getAllUsers();
-    return res.render('displayCRUD.ejs', {
-        dataTable: data
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUsers()
+  return res.render('displayCRUD.ejs', {
+    dataTable: data
+  })
+}
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId)
+    // check userData not found
+
+    // let userData
+    return res.render('editCRUD.ejs', {
+      user: userData
     })
+  } else return res.send('User not found!')
 }
 
-let getEditCRUD = async(req, res) => {
-    let userId = req.query.id;
-    if (userId) {
-        let userData = await CRUDService.getUserInfoById(userId);
-        // check userData not found
-
-        // let userData
-        return res.render('editCRUD.ejs', {
-            user: userData
-        });
-    } else
-        return res.send("User not found!");
+let putCRUD = async (req, res) => {
+  let data = req.body
+  let allUsers = await CRUDService.updateUserData(data)
+  return res.render('displayCRUD.ejs', {
+    dataTable: allUsers
+  })
 }
 
-let putCRUD = async(req, res) => {
-    let data = req.body;
-    let allUsers = await CRUDService.updateUserData(data);
-    return res.redirect("/get-crud");
-}
-
-let deleteCRUD = async(req, res) => {
-    let id = req.query.id;
-    if (id) {
-        await CRUDService.deleteUserById(id);
-        return res.send("Delete user success!");
-    } else
-        return res.send("User not found!");
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id
+  if (id) {
+    await CRUDService.deleteUserById(id)
+    return res.send('Delete user success!')
+  } else return res.send('User not found!')
 }
 
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage,
-    getCRUD: getCRUD,
-    postCRUD: postCRUD,
-    displayGetCRUD: displayGetCRUD,
-    getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD,
-    deleteCRUD: deleteCRUD,
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD
 }
--- Nodejs/src/services/userService.js
+++ Nodejs/src/services/userService.js
@@ -1,6 +1,19 @@
 import db from '../models/index'
 import bcrypt from 'bcryptjs'
 
+const salt = bcrypt.genSaltSync(10)
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var hashPassword = await bcrypt.hashSync(password, salt)
+      resolve(hashPassword)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
 let handleUserLogin = (email, password) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -90,7 +103,109 @@ let getAllUsers = (userId) => {
   })
 }
 
+let createNewUser = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Check email is exist or not
+      let check = await checkUserEmail(data.email)
+      if (check === true) {
+        resolve({
+          errCode: 1,
+          errMessage:
+            'Your email is already exist in your system. Please try other email!'
+        })
+      }
+
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+      await db.User.create({
+        email: data.email,
+        password: hashPasswordFromBcrypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        phonenumber: data.phonenumber,
+        gender: data.gender === '1' ? true : false,
+        roleId: data.roleId
+      })
+
+      resolve({
+        errCode: 0,
+        message: 'OK'
+      })
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+let deleteUser = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    let foundUser = await db.User.findOne({
+      where: {id: userId},
+      raw: false
+    })
+    if (!foundUser) {
+      resolve({
+        errCode: 2,
+        errMessage: `The user isn't exist`
+      })
+    }
+
+    await db.User.destroy({
+      where: {id: userId}
+    })
+
+    resolve({
+      errCode: 0,
+      message: 'The user is deleted'
+    })
+  })
+}
+
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.id) {
+        resolve({
+          errCode: 2,
+          errMessage: 'Missing required parameter'
+        })
+      }
+      let user = await db.User.findOne({
+        where: {id: data.id},
+        raw: false
+      })
+      if (user) {
+        user.firstName = data.firstName
+        user.lastName = data.lastName
+        user.address = data.address
+
+        await user.save()
+        // await db.User.save({
+        //   firstName: data.firstName,
+        //   lastName: data.lastName,
+        //   address: data.address
+        // })
+
+        resolve({
+          errCode: 0,
+          message: 'The user is updated successfully'
+        })
+      } else
+        resole({
+          errCode: 1,
+          errMessage: `The user isn't exist`
+        })
+    } catch {
+      reject(error)
+    }
+  })
+}
+
 module.exports = {
   handleUserLogin: handleUserLogin,
-  getAllUsers: getAllUsers
+  getAllUsers: getAllUsers,
+  createNewUser: createNewUser,
+  deleteUser: deleteUser,
+  updateUserData: updateUserData
 }
