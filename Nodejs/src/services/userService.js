819f43c3dee5ebe1fa3297aa9bb2e77295a0ccb8
Quoc Thang <comehere.thang@gmail.com>
2023-11-30 14:25:29 +0700

    Thêm Mới Người Dùng (CREATE)
    Thêm Mới Người Dùng (CREATE)

--- Nodejs/src/services/userService.js
+++ Nodejs/src/services/userService.js
@@ -114,24 +114,24 @@ let createNewUser = (data) => {
           errMessage:
             'Your email is already exist in your system. Please try other email!'
         })
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === '1' ? true : false,
+          roleId: data.roleId
+        })
+
+        resolve({
+          errCode: 0,
+          message: 'OK'
+        })
       }
-
-      let hashPasswordFromBcrypt = await hashUserPassword(data.password)
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === '1' ? true : false,
-        roleId: data.roleId
-      })
-
-      resolve({
-        errCode: 0,
-        message: 'OK'
-      })
     } catch (error) {
       reject(error)
     }
