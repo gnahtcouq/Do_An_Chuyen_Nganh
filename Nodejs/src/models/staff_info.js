2d5a9d14f1c5b89ed85d49273557fcc72c879f47
Quoc Thang <comehere.thang@gmail.com>
2023-12-31 19:04:55 +0700

    Thêm Giao Diện Lưu Thông Tin Nhân viên và Table Staff Infor
    Thêm Giao Diện Lưu Thông Tin Nhân viên và Table Staff Infor

---
+++ Nodejs/src/models/staff_info.js
@@ -0,0 +1,28 @@
+'use strict'
+const {Model} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Staff_Info extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Staff_Info.init(
+    {
+      staffId: DataTypes.INTEGER,
+      priceId: DataTypes.STRING,
+      paymentId: DataTypes.STRING,
+      note: DataTypes.STRING,
+      count: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Staff_Info'
+    }
+  )
+  return Staff_Info
+}
